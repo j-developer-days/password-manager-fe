03ec8c2d3850df8864c65cae92ca79410d272919
j_dev <j.developer.days@gmail.com>
2023-05-20 12:03:46 +0300

    adding navigation

--- password-manager-fe/src/app/app-routing.module.ts
+++ password-manager-fe/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './components/account/account.component';
+import { GroupAccountComponent } from './components/group-account/group-account.component';
+import { AppComponent } from './components/app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accounts', component: AccountComponent },
+  { path: 'group-accounts', component: GroupAccountComponent },
+  { path: 'home', component: AppComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
